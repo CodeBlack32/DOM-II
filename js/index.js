5f4ceb54058df1b237abbe6ed1378b6135f2db52
Jarvis Black <usnblack@gmail.com>
2019-12-08 22:22:57 -0500

    Finished MVP

--- js/index.js
+++ js/index.js
@@ -1,3 +1,77 @@
 // Your code goes here
 //Starting
 
+//Mouse event
+//Home button only
+const navLink = document.querySelector('.nav-link');
+navLink.addEventListener('click', function( stop ) {
+    console.log('I was clicked')
+    stop.preventDefault();
+});
+
+const rightClick = document.querySelector('h1'); 
+rightClick.addEventListener('contextmenu', function() {
+    rightClick.style.display = 'none'
+    console.log('Right Mouse Button Clicked!')
+});
+
+const targetDblClick = document.querySelector('body'); 
+targetDblClick.addEventListener('dblclick', function( event ) {
+    event.target.style.color = 'blue';
+});
+
+document.addEventListener('mousemove', function() {
+    console.log('The Mouse is Moving!');
+});
+
+//first button
+const btnHover = document.querySelector('.btn'); 
+btnHover.addEventListener('mouseover', function() {
+ btnHover.style.color = 'orange';
+});
+
+//first text content section
+const mouseEnter = document.querySelector('.text-content'); 
+mouseEnter.addEventListener('mouseenter', function() {
+ mouseEnter.style.color = 'purple';
+});
+
+//first h4
+const mouseExit = document.querySelector('h4'); 
+mouseExit.addEventListener('mouseleave', function(prop) {
+ mouseExit.style.fontSize = '80px';
+});
+
+//stop Propagation
+const divMouse = document.querySelector('h2'); 
+divMouse.addEventListener('mouseleave', function( prop ) {
+ divMouse.style.fontSize = '80px';
+ prop.stopPropagation();
+});
+
+//first div only
+const divChange = document.querySelector('.text-content'); 
+divChange.addEventListener('click', function( event ) {
+ divChange.style.background = 'red';
+});
+
+
+
+//Keyboard event
+const keyRecord = document.querySelector('html')
+keyRecord.addEventListener('keydown', function() {
+ console.log('You pressed the keyboard');
+});
+
+const keyRelease = document.querySelector('html')
+keyRecord.addEventListener('keyup', function() {
+ console.log('You let up on the keyboard');
+});
+
+
+
+//other events
+window.addEventListener('scroll', function() {
+    console.log('You Scrolled!');
+});
+
